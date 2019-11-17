639287548042ac8921367eb62c145537061cde95
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-17 09:31:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b| b <=> a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort{|a,b|
+  a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+  array
+end
+
+def kesha_maker(array)
+  array.each{|n| n[2] = "$"}
+end
+
+def find_a(array)
+  array.select{|n| n.start_with?("a")}
+end
+
+def sum_array(array)
+  array.sum #each{|n| n.sum  }
+end
